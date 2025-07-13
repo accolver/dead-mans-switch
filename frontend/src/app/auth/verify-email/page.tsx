3eaeb895767bb1c2b3cf71b02a02771d7112a431
Alan Colver <accolver@gmail.com>
2025-07-13 16:28:00 -0600

    Center email verify text

--- frontend/src/app/auth/verify-email/page.tsx
+++ frontend/src/app/auth/verify-email/page.tsx
@@ -1,13 +1,15 @@
 export default function VerifyEmailPage() {
   return (
-    <div>
-      <h2 className="text-center text-3xl font-bold tracking-tight">
-        Check your email
-      </h2>
-      <p className="text-muted-foreground mt-2 text-center text-sm">
-        We've sent you a verification link. Please check your email to verify
-        your account.
-      </p>
+    <div className="flex min-h-screen items-center justify-center">
+      <div>
+        <h2 className="text-center text-3xl font-bold tracking-tight">
+          Check your email
+        </h2>
+        <p className="text-muted-foreground mt-2 text-center text-sm">
+          We've sent you a verification link. Please check your email to verify
+          your account.
+        </p>
+      </div>
     </div>
   )
 }
